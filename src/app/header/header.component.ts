56c83217d65e666a7ba1b96b6f0e258adb955a66
Astrophel <astroolabe@gmail.com>
2024-10-01 23:14:46 +0200

    header v1.5

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import {
+  faTwitter,
+  faBluesky
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatTooltipModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatTooltipModule, FontAwesomeModule, ClipboardModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faTwitter,
+      faBluesky
+    );
+  }
+
+  getURL() {
+    return window.location.href;
+  }
 }
